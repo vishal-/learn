0509babb46b3422afabe36e241a238b384531a02
Vishal Gupta <ims.vishal@gmail.com>
2024-03-20 10:39:36 +0530

    Added db configurations

--- apis/src/app.server.ts
+++ apis/src/app.server.ts
@@ -1,20 +1,16 @@
 import express, { Application, Response } from "express";
-// import bodyParser from "body-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-const SERVER_PORT = 3030;
-console.log(process.env.DB_HOST);
-
-// const cors = require("cors");
-
-// const { env } = process;
+const { env } = process;
 
 class Server {
   app: Application;
 
   constructor() {
     this.app = express();
-    // this.app.use(bodyParser.json());
-    // this.app.use(cors());
+    this.app.use(bodyParser.json());
+    this.app.use(cors());
   }
 
   serve = () => {
@@ -22,8 +18,8 @@ class Server {
       response.send("it works");
     });
 
-    this.app.listen(SERVER_PORT, () => {
-      console.log(`server started at port ... ${SERVER_PORT}`);
+    this.app.listen(env.APP_PORT, () => {
+      console.log(`server started at port ... ${env.APP_PORT}`);
     });
   };
 }
